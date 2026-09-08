04136bf8b32b01c846d98ecc5dc95fed3f2172e4
drajat824 <drajat.fikri24@gmail.com>
2026-09-08 16:35:36 +0700

    feat: integrate Bluetooth scanning functionality in DashboardSmartwatch component; request permissions and manage device scanning

--- src/app/(other)/dashboard_smartwatch.tsx
+++ src/app/(other)/dashboard_smartwatch.tsx
@@ -1,23 +1,23 @@
 import MaterialDesignIcons from "@react-native-vector-icons/material-design-icons";
-import { useRouter } from "expo-router";
 import { useEffect, useRef, useState } from "react";
-import { Animated, Easing, Pressable, Text, View } from "react-native";
-import { Switch } from "react-native-paper";
+import { Animated, Easing, PermissionsAndroid, Platform, Pressable, Text, View } from "react-native";
+import { BleManager, Device } from "react-native-ble-plx";
 import { Cards, RouterSub, WrapperMain } from "../../component";
 
+const bleManager = new BleManager();
+
 export default function DashboardSmartwatch() {
-  const router = useRouter();
+  const [isBlePoweredOn, setIsBlePoweredOn] = useState(true);
+  const onToggleSwitch = () => setIsBlePoweredOn(!isBlePoweredOn);
 
-  const [isSwitchOn, setIsSwitchOn] = useState(true);
-  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+  const [devices, setDevices] = useState<Device[]>([]);
+  const [isScanning, setIsScanning] = useState(false);
 
-  const [isRefresh, setIsRefresh] = useState(false);
   const refreshAnimation = useRef(new Animated.Value(0)).current;
-
   const AnimatedMaterialIcon = Animated.createAnimatedComponent(MaterialDesignIcons);
 
   useEffect(() => {
-    if (!isRefresh) {
+    if (!isScanning) {
       refreshAnimation.stopAnimation();
       refreshAnimation.setValue(0);
       return;
@@ -35,13 +35,49 @@ export default function DashboardSmartwatch() {
     animation.start();
 
     return () => animation.stop();
-  }, [isRefresh, refreshAnimation]);
+  }, [isScanning, refreshAnimation]);
 
-  const handleRefresh = () => {
-    setIsRefresh(true);
+  const requestAndroidPermissions = async () => {
+    if (Platform.OS === "android") {
+      if (Platform.Version >= 31) {
+        const granted = await PermissionsAndroid.requestMultiple([PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN, PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT, PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION]);
+        return granted["android.permission.BLUETOOTH_SCAN"] === PermissionsAndroid.RESULTS.GRANTED && granted["android.permission.BLUETOOTH_CONNECT"] === PermissionsAndroid.RESULTS.GRANTED;
+      } else {
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      }
+    }
+    return true;
+  };
+
+  const startScan = async () => {
+    const hasPermission = await requestAndroidPermissions();
+    if (!hasPermission) return;
+
+    setDevices([]);
+    setIsScanning(true);
+
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.error(error);
+        setIsScanning(false);
+        return;
+      }
+
+      if (device && device.name) {
+        setDevices((prevDevices) => {
+          if (!prevDevices.some((d) => d.id === device.id)) {
+            return [...prevDevices, device];
+          }
+          return prevDevices;
+        });
+      }
+    });
+
     setTimeout(() => {
-      setIsRefresh(false);
-    }, 5000);
+      bleManager.stopDeviceScan();
+      setIsScanning(false);
+    }, 10000);
   };
 
   return (
@@ -52,12 +88,12 @@ export default function DashboardSmartwatch() {
 
         {/* FLEX 2: CARDS CONTENT  */}
         <View className="flex-1 flex-col gap-6 mt-4">
-          <View className="flex flex-row justify-between">
-            <View className="flex flex-col gap-2 items-start">
-              <Switch className="ml-[-10]" color="#017BFE" value={isSwitchOn} onValueChange={onToggleSwitch} />
+          <View className="flex flex-row justify-start">
+            {/* <View className="flex flex-col gap-2 items-start">
+              <Switch className="ml-[-10]" color="#017BFE" value={isBlePoweredOn} onValueChange={onToggleSwitch} />
               <Text className="text-label">BLUETOOTH AKTIF</Text>
-            </View>
-            <Pressable className="flex flex-row gap-2 items-center active:opacity-50" onPress={handleRefresh}>
+            </View> */}
+            <Pressable className="flex flex-row gap-2 items-center active:opacity-50" onPress={startScan}>
               <AnimatedMaterialIcon
                 name="refresh"
                 size={45}
@@ -76,6 +112,7 @@ export default function DashboardSmartwatch() {
               <Text className="text-normal text-theme-blue font-semibold">SCAN</Text>
             </Pressable>
           </View>
+
           {/* LIST DEVICES SMARTWATCH  */}
           <View className="flex-col gap-4">
             <Cards pressable onPress={() => console.log("TES")} color="#017BFE80" className="flex flex-row justify-between items-center">
