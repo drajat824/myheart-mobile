0ccec8d71d13971f317950e70145c9b4a3ae4f71
drajat824 <drajat.fikri24@gmail.com>
2026-09-19 09:10:41 +0700

    feat: add support for glTF and GLB assets, update dependencies, and create apiService utility
    - Updated metro.config.js to support "glb" and "gltf" asset extensions.
    - Added expo-gl and expo-three dependencies to package.json.
    - Included @types/three for TypeScript support.
    - Created a new apiService.tsx file for API utility functions.
    - Updated yarn.lock to reflect new dependencies and their versions.

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativewind } = require("nativewind/metro");
- 
+
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
- 
+config.resolver.assetExts.push("glb", "gltf");
+
 module.exports = withNativewind(config);
